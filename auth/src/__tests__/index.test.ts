578cc15f6b751b9f1cee035c502f1691efc4a5e8
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2020-01-08 21:19:23 +0100

    Initial commit

--- auth/src/__tests__/index.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import { app } from '../app';
-
-it('Respond with status code 200', async () => {
-    await request(app)
-        .get('/')
-        .expect(200);
-});
-
